a64ec34adb65f078e915dc56f7c9fb454809aafa
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2022-07-29 13:27:20 +0300

    Lisätty pytest ja hakemisto yksikkötesteille

---
+++ src/tests/connect4_test.py
@@ -0,0 +1,13 @@
+import unittest
+from connect4 import ConnectFour
+
+class TestConnectFour(unittest.TestCase):
+    def setUp(self):
+        self.connect4 = ConnectFour()
+        self.board = self.connect4.init_board(6,7)
+        
+    def test_column_is_available_returns_False_if_column_is_full(self):
+        # Täytetään neljäs sarake
+        self.pelilauta[:, 3] = 1
+
+        self.assertFalse(self.connect4.column_is_available(self.pelilauta, 4))
