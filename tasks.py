7dfcaf363e097b81a07c4fd6e65ca2b6de3e7d89
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2022-08-24 14:38:40 +0300

    Invoken hitausongelma korjattu

--- tasks.py
+++ tasks.py
@@ -2,13 +2,13 @@ from invoke import task
 
 @task
 def play(c):
-    c.run("poetry run python3 src/play.py")
+    c.run("python3 src/play.py", pty=True)
 
 @task
 def test(c):
-    c.run("poetry run pytest")
+    c.run("pytest", pty=True)
 
 @task
 def coverage(c):
-    c.run("poetry run coverage run --branch -m pytest")
-    c.run("poetry run coverage html")
+    c.run("coverage run --branch -m pytest", pty=True)
+    c.run("coverage html", pty=True)
