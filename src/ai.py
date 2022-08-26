e64b9d4d6a282b94156b2f635e79c5318016aa73
Rasmus Heikkinen <rasmus.heikkinen@helsinki.fi>
2022-08-26 22:16:36 +0300

    AI-luokkaa refaktoroitu

--- src/ai.py
+++ src/ai.py
@@ -1,5 +1,4 @@
 from math import inf
-from random import randint
 from time import time
 from constants import ROW_COUNT, COL_COUNT
 from gameboard import GameBoard
@@ -8,7 +7,7 @@ MAX_SCORE = 10_000_000
 MIN_SCORE = -10_000_000
 
 class AI:
-    def __init__(self, level=1, player=2):
+    def __init__(self, level=2, player=2):
         self.level = level
         self.ai_player = player
         self.opponent = (player % 2) + 1
@@ -18,45 +17,42 @@ class AI:
     def player(self):
         return self.ai_player
 
-    def best_column(self, board: GameBoard, depth=7):
-        """Valitsee pelitekoälylle seuraavaksi tehtävän siirron
-        määritetyn vaikeustason (level) mukaisella menetelmällä.
+    def best_column(self, board: GameBoard, depth: int = 7):
+        """Valitsee pelitekoälylle seuraavaksi tehtävän siirron käyttäen
+        minimax-algoritmia. Tekoälylle määritetty vaikeustaso (level) vaikuttaa
+        laskentasyvyyteen eli siihen, kuinka monta siirtoa algoritmi laskee eteenpäin.
 
         :param board: Peliruudukko GameBoard-luokan oliona
+        :param depth: Laskentasyvyys minimax algoritmille. Jos tyhjä, käytetään oletussyvyyttä 7.
 
         :rtype: tuple
         :return: Palauttaa valitun sarakkeen indeksin, siirrolle lasketun pisteytyksen
         ja laskennan suoritusajan sekunteina.
         """
         if self.level == 0:
-            # Palauttaa satunnaisen sarakkeen väliltä [0, sarakkeiden_lkm - 1]
-            column = randint(0, COL_COUNT-1)
-
+            depth = 0
         if self.level == 1:
-            # Asetetaan käsiteltyjen solmujen lukumäärä nollaksi
-            self.nodes_visited = 0
-            start_time = time()
+            depth = 4
 
-            # Alfan ja betan alkuarvot
-            alpha, beta = -inf, inf
+        self.nodes_visited = 0
 
-            value, column = self.minimax(board, alpha, beta, depth, maximizing=False)
+        alpha, beta = -inf, inf # Alfan ja betan alkuarvot
 
-            end_time = time()
-            runtime = end_time - start_time
+        start_time = time()
+        value, column = self.minimax(board, alpha, beta, depth, maximizing=False)
+        runtime = time() - start_time
 
-            print(f"Pelipuun solmuja käsitelty: {self.nodes_visited} kpl")
-            print(f"Minimax-algoritmin suoritusaika: {runtime} sekuntia\n")
-            print(f"Minimax valitsi sarakkeen {column + 1} pisteytyksellä {value}\n")
-
+        print(f"Pelipuun solmuja käsitelty: {self.nodes_visited} kpl")
+        print(f"Minimax-algoritmin suoritusaika: {runtime} sekuntia\n")
+        print(f"Minimax valitsi sarakkeen {column + 1} pisteytyksellä {value}\n")
 
         return column, value, runtime
 
     def minimax(self, board: GameBoard, alpha, beta, depth: int, maximizing: bool):
         """Minimax-algoritmi alfa-beta -karsinnalla. Käy rekursiivisesti pelipuuta läpi
-        päätössolmuun tai annettuun syvyyteen asti. Pelitekoäly on algoritmissa minimoiva
-        pelaaja, eli algoritmi palauttaa sitä pienemmän pisteytyksen mitä parempi pelitilanne
-        on tekoälypelaajan kannalta.
+        päätössolmuun (havaittu voitto tai tasapeli) tai annettuun syvyyteen asti.
+        Pelitekoäly on algoritmissa minimoiva pelaaja, eli algoritmi palauttaa sitä
+        pienemmän pisteytyksen mitä parempi pelitilanne on tekoälypelaajan kannalta.
 
         :param board: Peliruudukko GameBoard-luokan oliona
         :param alpha: Alfan arvo
@@ -72,14 +68,22 @@ class AI:
         if board.board_is_full(): # Päätössolmu, tasapeli
             return 0, None
 
+        player_in_turn = self.opponent if maximizing else self.ai_player
+
+        if self.player_wins_next_move(board, player_in_turn):
+            if maximizing:
+                # Maksimoivan pelaajan voitto
+                win_value = MAX_SCORE * (depth+1)
+            else:
+                # Minimoivan pelaajan voitto
+                win_value = MIN_SCORE * (depth+1)
+            return win_value, self._winning_move
+
         if depth == 0: # Laskentasyvyys saavutettu
             value = self.evaluate_board(board)
             return value, None
 
         if maximizing: # Maksimoiva pelaaja
-            if self.player_wins_next_move(board, self.opponent):
-                return MAX_SCORE*depth, self._winning_move
-
             max_value = -inf
             valid_columns = board.get_available_columns()
             best_column = valid_columns[0]
@@ -104,9 +108,6 @@ class AI:
             return max_value, best_column
 
         # Minimoiva pelaaja (tekoäly)
-        if self.player_wins_next_move(board, self.ai_player):
-            return MIN_SCORE*depth, self._winning_move
-
         min_value = inf
         valid_columns = board.get_available_columns()
         best_column = valid_columns[0]
@@ -144,25 +145,20 @@ class AI:
         for column in valid_moves:
             self.nodes_visited += 1
             row = board.get_next_available_row(column)
-            # Peliruudukkoon ei tehdä muutoksia, riittää tallettaa siirron tiedot GameBoard-oliolle
-            board.store_last_move(row, column, player)
-            if board.check_for_win():
+            if board.check_for_win(row, column, player):
                 self._winning_move = column
                 return True
         return False
 
     def evaluate_board(self, board: GameBoard):
-        """Pisteytysfunktio, joka käy läpi koko peliruudukon ja pisteyttää pelitilanteen vuorossa
-        olevan pelaajan kannalta.
+        """Pisteytysfunktio, joka käy läpi koko peliruudukon ja pisteyttää pelitilanteen
+        tekoälypelaajan (minimoiva pelaaja) kannalta.
 
         :param board: Peliruudukko GameBoard-luokan oliona
 
         :rtype: int
         :return: Palauttaa pelitilanteelle lasketun pisteytyksen
         """
-        # Viimeisimmän siirron tehnyt pelaaja
-        _, _, player = board.get_last_move()
-
         value = 0
 
         # VAAKASUUNTAISET NELJÄN RUUDUN LOHKOT
@@ -171,7 +167,7 @@ class AI:
             for column in range(COL_COUNT - 3):
                 evaluation_block = [board.get_position(row, val) for val in range(column, column+4)]
 
-                value += self.get_block_value(evaluation_block, player)
+                value += self.get_block_value(evaluation_block)
 
         # PYSTYSUUNTAISET NELJÄN RUUDUN LOHKOT
 
@@ -179,7 +175,7 @@ class AI:
             for row in range(ROW_COUNT - 3):
                 evaluation_block = [board.get_position(val, column) for val in range(row, row + 4)]
 
-                value += self.get_block_value(evaluation_block, player)
+                value += self.get_block_value(evaluation_block)
 
         # VINOTTAISET NELJÄN RUUDUN LOHKOT
 
@@ -188,49 +184,42 @@ class AI:
             for col in range(COL_COUNT - 3):
                 evaluation_block = [board.get_position(row+val, col+val) for val in range(4)]
 
-                value += self.get_block_value(evaluation_block, player)
+                value += self.get_block_value(evaluation_block)
 
         # Laskeva suunta (\)
         for row in range(ROW_COUNT - 1, ROW_COUNT - 4, -1):
             for col in range(COL_COUNT - 3):
                 evaluation_block = [board.get_position(row-val, col+val) for val in range(4)]
 
-                value += self.get_block_value(evaluation_block, player)
+                value += self.get_block_value(evaluation_block)
 
-        # Jos pelaaja on tekoäly eli minimoiva pelaaja, palautetaan saadun arvon vastaluku
-        if player == self.ai_player:
-            return -value
+        return -value
 
-        return value
-
-    def get_block_value(self, block: list, player: int):
-        """Pisteyttää neljän ruudun mittaisen lohkon vuorossa olevan pelaajan kannalta.
+    def get_block_value(self, block: list):
+        """Pisteyttää neljän ruudun kokoisen lohkon tekoälypelaajan kannalta.
         Lohko on lista, joka sisältää neljän peräkkäisen ruudun arvot vaaka-, pysty-
         tai vinosuunnassa.
 
         :param block: Pisteytettävä lohko
-        :param player: Vuorossa oleva pelaaja, 1 tai 2
 
         :rtype: int
         :return: Palauttaa lohkon pisteytyksen
         """
-        opponent = (player % 2) + 1
-
-        player_discs = 0
+        ai_discs = 0
         opponent_discs = 0
         empty_count = 0
 
         for disc in block:
-            if disc == player:
-                player_discs +=1
-            elif disc == opponent:
+            if disc == self.ai_player:
+                ai_discs +=1
+            elif disc == self.opponent:
                 opponent_discs += 1
             else: # Ruutu on tyhjä
                 empty_count += 1
 
-        if player_discs == 3 and empty_count == 1:
+        if ai_discs == 3 and empty_count == 1:
             return 10
-        if player_discs == 2 and empty_count == 2:
+        if ai_discs == 2 and empty_count == 2:
             return 4
         if opponent_discs == 3 and empty_count == 1:
             return -10
